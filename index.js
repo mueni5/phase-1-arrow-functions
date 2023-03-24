51209efbadcacb25596a5f878667553a1ec01c15
+ <+>
2023-03-24 08:52:07 +0300

    Completedassignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(para1, para2) {
+    return (2000/100)
+    
+}
+
+const square = (para1,para2)=>para1*para1
+let para1 = 2;
+square(2*2)
+
+
+const add = (para1,para2) =>para1+para2
